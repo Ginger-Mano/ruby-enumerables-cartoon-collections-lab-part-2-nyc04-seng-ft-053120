d6ccc1f4e5f7b01ba889b49e82aa336d53d4b354
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-11 13:37:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.include? do |element|
+  planeteer_calls.find do |element|
     if planeteer_calls == element
       element
     else
